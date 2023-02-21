330d895a986bf765200873f341e2c49c419e2a60
Areebey <areebrafiq125@gmail.com>
2023-02-22 01:41:45 +0500

    final commit

--- Zodiac  Lucky  Color/script.js
+++ Zodiac  Lucky  Color/script.js
@@ -12,37 +12,39 @@ const changeBackground=()=>{
         body.style.backgroundColor  = "#a432d1";
             break;
             case "gemini":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#e3ac17";
             break;
             case "cancer":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#a432d1";
             break;
             case "Leo":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#e3ac17";
             break;
             case "virgo":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#a432d1";
             break;
             case "libra":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#e3ac17";
             break;
             case "scorpio":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#a432d1";
             break;
             case "sagittarius":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#e3ac17";
             break;
             case "Capricorn":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#a432d1";
             break;
             case "aquarius":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#e3ac17";
             break;
             case "pisces":
-        body.style.backgroundColor = "";
+        body.style.backgroundColor = "#a432d1";
             break;
     
         default:
+       body.style.backgroundColor = "#ffff";
+
             break;
     }
 
