bf804345f580ce17b1bfa682400a49eb99babf59
Areebey <areebrafiq125@gmail.com>
2023-02-22 01:32:33 +0500

    sec commit

--- Zodiac  Lucky  Color/script.js
+++ Zodiac  Lucky  Color/script.js
@@ -3,4 +3,19 @@ const zodiac=document.getElementById("zodiac");
 
 const changeBackground=()=>{
     console.log(zodiac.value)
+
+    switch (zodiac.value) {
+        case "aries":
+        body.style.backgroundColor = "#e3ac17";
+            break;
+        case "taurus":
+        body.style.backgroundColor  = "#a432d1";
+            break;
+            
+    
+        default:
+            break;
+    }
+
+
 }
