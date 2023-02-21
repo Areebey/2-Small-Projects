178ca386938f625f851368132298f097ba4ea20a
Areebey <areebrafiq125@gmail.com>
2023-02-22 02:39:16 +0500

    final commit

---
+++ Name Tag/script.js
@@ -0,0 +1,17 @@
+// function
+
+const name=document.getElementById("name");
+const tagsCon=document.querySelector(".tags")
+
+const addName = () =>{
+   if(name.value === ""){
+    return null;
+   }
+
+    const tag=document.createElement("div")
+    tag.setAttribute("class","tag");
+
+    tag.innerText = "Hey, I am "+name.value;
+
+    tagsCon.append(tag);
+}
